8b9c7a87b6aee80608490cb4431bb8d30aff50e2
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-20 05:18:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,8 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      
+      item = req.path.split("/items/").last 
+      if item 
     else
       resp.write "Route not found"
       resp.status = 404
